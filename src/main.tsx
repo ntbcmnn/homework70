0ad74fb912f955d8e63896288443ce0ff7706d7d
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-16 15:50:29 +0600

    Added bootstrap-icons deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
